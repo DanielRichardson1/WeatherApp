e0b7e8e0ee61496210249545b1b6b6e9b59ae990
danielrichardson1 <drich1475@gmail.com>
2024-12-29 22:24:58 -0500

    Finalized Basic Location-based Weather UI

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     var body: some View {
         VStack {
             
             if let location = locationManager.location {
-                Text("Your coordinates are \(location.latitude), \(location.longitude)")
-            } else{
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                try await
+                                weather = weatherManager.getCurrentWeather(
+                                    latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error fetching weather: \(error)")
+                            }
+                        }
+                }
+            } else {
                 if locationManager.isLoading {
                     LoadingView()
                 } else {
